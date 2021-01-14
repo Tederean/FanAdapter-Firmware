6eb7aa63b80539e8593808163686bb5972b90208
tederean <tederean@gmail.com>
2021-01-14 20:36:29 +0100

    Outsource TedereanFramework

--- src/application/common/FanRegulator.cpp
+++ src/application/common/FanRegulator.cpp
@@ -11,7 +11,7 @@ namespace FanRegulator
 
   uint8_t Step(uint8_t targetPower, uint8_t currentPower);
 
-  FastPID PID(0.0f, 0.1f, 0.0f, TickFrequency, 8);
+  //FastPID PID(0.0f, 0.1f, 0.0f, TickFrequency, 8);
 
   void Initialize(float tickFrequency)
   {
